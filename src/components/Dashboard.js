b8427983548a924f65b2e7e2a2574e40f8ef2d8d
km1326 <komal.raj@khelomore.com>
2022-02-28 23:22:44 +0530

    Changes for dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { useAPI } from "../contexts";
+import PostCard from "./PostCard";
 
 const Dashboard = () => {
+  const apiData = useAPI();
+
   return (
-    <div>Dashboard</div>
+    <div className="container">
+      {
+        apiData?.isLoading ? <div className="loading">Loading...</div> :
+          <div>
+            {
+              apiData?.posts?.length ? <div className="post-list">
+                {apiData?.posts?.map(post => <PostCard post={post} />)}
+              </div> :
+                <div className="no-post-msg">
+                  <span>Uh-Oh! </span>
+                  <p>No post available</p>
+                </div>
+            }
+          </div>
+      }
+    </div>
   )
 };
 
